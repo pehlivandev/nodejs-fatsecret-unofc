27a2f6cfa58aaf5984b741c3540e5665960d243f
pehlivandev <pehlivandev@gmail.com>
2024-06-06 00:43:06 +0300

    detail page endpoint created

--- index.js
+++ index.js
@@ -12,10 +12,32 @@ app.get('/', (req, res) => {
         <title>Playwright Uygulaması</title>
       </head>
       <body>
-        Hello world!
+        Hello world! <br />
+        <button id="sendButton">Gönder</button>
+        <script type="text/javascript">
+          const button = document.getElementById('sendButton')
+          button.addEventListener('click', function() {
+            fetch('/detail', { method: 'POST' }).then(
+              response => {
+                console.log('RESPONSE: ', response)
+                return response
+              },
+              error => {
+                console.log('ERROR: ', error)
+                return error
+              }
+            )
+          })
+        </script>
       </body>
     </html>
   `)
+})
+
+app.post('/detail', (req, res) => {
+  res.status(200).json({
+    result: 'detail page dom'
+  })
 })
 
 app.listen(3000)
